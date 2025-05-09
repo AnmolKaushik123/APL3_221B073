aab9eaca75028442e2b53d6f385720a567510ddd
AnmolKaushik123 <221b073@juetguna.in>
2025-05-09 09:22:10 +0530

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Offering o = new Coffee();
-        o = new Rum(o);
-        o = new Rum(o);
-        System.out.println(o.getName() + " " + o.getPrice() );
-    }
-}
